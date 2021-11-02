cd837f6aad5286fed85414211cd74ae7ed6da82a
Razfan <stefan.r95@hotmail.com>
2021-11-02 18:15:21 +0100

    Tercer Commit

--- Ejercicio1_1.php
+++ Ejercicio1_1.php
@@ -3,19 +3,22 @@
 
 if (isset($_POST["Enviar"])) { //Recogemos los datos al pulsar el boton enviar
     $calculo = $_POST["Coe1"] * $_POST["Coe1"] - 4 * $_POST["Coe1"] * $_POST["Coe3"];
-
+    echo $calculo;
+    // 1 -2 1
+    // 3 -3 2
     if ($calculo < 0) {
         echo "No existen raices reales :(";
     } else if ($calculo = 0) {
-        $calculo = -$_POST["Coe2"] / 2 * $_POST["Coe1"];
+        $calculo = $_POST["Coe2"] / 2 * $_POST["Coe1"];
         $ecua2;
-        $ecua2 = -$_POST["Coe2"] / 2 * $_POST["Coe1"];
+        $ecua2 = $_POST["Coe2"] / 2 * $_POST["Coe1"];
         $ecu1 = $ecua2;
         echo $ecua1 . "</br>" . $ecua2 . "</br>";
     } else if ($calculo > 0) {
         $ecua1 = -$_POST["Coe2"] + sqrt($calculo) / 2 * -$_POST["Coe2"] / 2 * $_POST["Coe1"];
         $ecua2 = -$_POST["Coe2"] - sqrt($calculo) / 2 * -$_POST["Coe2"] / 2 * $_POST["Coe1"];
         echo $ecua1 . "</br>" . $ecua2 . "</br>";
+        echo " funciona";
     }
 } else {
     echo "<b>  No puede estar vacío ningun Coeficiente. </b><br/>";
